55022c45ffc5d5e3f1845c427cdf2f1ed8adc67e
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-02-14 14:43:06 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+
+var contador = 1
+var contador2 = 1
+while (contador <= 4){
+    console.log(`passo ${contador} while`)
+    contador++
+}
+
+do {
+    console.log(`passo ${contador2} do`)
+    contador2++
+} while(contador2 < 8)
