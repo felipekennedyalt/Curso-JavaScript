02c74a84132af3ffb78ac04062d9b22270cabe40
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-02-25 14:48:07 -0300

    funcões

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parImpar(n){
+if(n%2==0){
+return 'par!'
+}else{
+    return 'Impar!'
+}
+}
+
+let res = parImpar(50)
+console.log(res)
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1 = 0, n2 = 0){ //se não passar o n1 ou o n2 eles vão valer 0, que foi o valor que eu defini como padrão caso não haja input
+    return n1 + n2
+}
+
+console.log(soma(5,7))
