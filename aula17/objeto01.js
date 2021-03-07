435c7566457e508ac877d7020fd565f441a02e25
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-03-07 13:45:10 -0300

    ultima aula

---
+++ aula17/objeto01.js
@@ -0,0 +1,10 @@
+let amigo = {nome: 'José', 
+sexo: 'M', 
+peso: 85.5,
+engordar(p=0){
+console.log('engordou')
+this.peso += p
+}}
+
+amigo.engordar(2)//vai engordar 2 kilos
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
